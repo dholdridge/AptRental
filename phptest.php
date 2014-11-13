55f8dcd36fb688f5baed3eb7481ecf05f7ee97ee
Dan Holdridge <dholdridge3@gmail.com>
2014-11-13 16:04:22 -0500

    Added sample of PHP code

---
+++ phptest.php
@@ -0,0 +1,23 @@
+<!-- Test PHP Script -->
+<html> <!-- Start out with normal HTML style tags -->
+	<head>
+		<title>Test</title>
+	</head>
+	<body>
+	</p>Testing PHP</p>
+	<!-- Begin parsing as PHP -->
+	<?php /* This tag starts PHP code */
+		//PHP uses C-style comments
+		function test_func() { // Defines a function called test_func
+			echo '<p>Should output 42</p>\n';
+			/* Echo causes the argument to be output to the HTML,
+			So the above line will appear in the HTML source */
+			$num = 42; // PHP variables begin with $ and are dynamically-typed
+						//Sorry Jamie :(
+			echo '<p>$num</p>\n';
+		}
+		test_func();
+	?> <!-- Close the php tag -->
+	<p>End Test</p>
+	</body>
+</html>

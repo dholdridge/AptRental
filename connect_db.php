f922ec684ec3535e90c29904a536b8c5912c50fa
Dan Holdridge <dholdridge3@gmail.com>
2014-11-19 14:39:50 -0500

    Fixed some errors in connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,6 @@
 <?php 
 	function connect_db() {
-		@ $db = new mysqli('academic-mysql.cc.gatech.edu', 'cs4400_Group_56', 'TZnYYMN9', 'cs4400_Group_56')
+		@ $db = new mysqli('academic-mysql.cc.gatech.edu', 'cs4400_Group_56', 'TZnYYMN9', 'cs4400_Group_56');
 		if (mysqli_connect_errno() ) {
 			echo "Error: couldn't connect to database";
 			exit;
@@ -14,10 +14,10 @@
 	$result = $db->query($query);
 	$num_results = $result->num_rows;
 	
-	for ($i=0;i<$num_results;i++) {
+	for ($i=0;i<$num_results;$i++) {
 		$row = $result->fetch_assoc();
-		echo "$row['Username'], $row['Resident_Name'], $row['Date_Of_Birth'], $row['Apt_No']<br \>";
+		echo $row['Username'].', '.$row['Resident_Name'].", ".$row['Date_Of_Birth'].', '.$row['Apt_No']."<br \>";
 	}
-	$result->free()
-	$db->close()
+	$result->free();
+	$db->close();
 	?>

439cd000e4a924de6533650eaf2c22d4db66b030
Stephen Dao <daogatech@localhost.localdomain>
2014-11-30 01:02:20 -0500

    add application, reminder, leasingreport and resolution

--- application.php
+++ application.php
@@ -13,13 +13,17 @@ function popup2() {
 Name: <input type="text" name="uname"><br>
 Birthday: <input type="date" name="birthday"><br>
 Gender: <select name="gender">
-<option value="male">Male</option>
-<option value="female">Female</option></select><br>
+<option value="M">M</option>
+<option value="F">F</option></select><br>
 Income: <input type="text" name="income"><br>
+Preferred Lease Term: <select name="leaseterm">
+<option value="3">3</option>
+<option value="6">6</option>
+<option value="12">12</option></select><br>
 Apartment Category: <select name="apt_cat">
-<option value="male">1bdr-1bth,</option>
-<option value="female">2bdr-1bth</option>
-<option value="female">2bdr-2bth</option></select><br>
+<option value="1bdr-1bth">1bdr-1bth</option>
+<option value="2bdr-1bth">2bdr-1bth</option>
+<option value="2bdr-2bth">2bdr-2bth</option></select><br>
 Preferred Move In Date: <input type="date" name="movein"><br>
 Previous Residence: <input type="text" name="prev"><br>
 Rent Minimum: <input type="text" name="min"><br>
@@ -34,21 +38,21 @@ Rent Maximum: <input type="text" name="max"><br>
 <?php
 include 'dbinfo.php' ; 
 if (isset($_POST['submit'])) {
-    if ($_POST['uname'] == "" || $_POST['birthday'] == "" || $_POST['gender'] == "" || $_POST['income'] == "" || $_POST['apt_cat'] == "" || $_POST['movein'] == "" || $_POST['prev'] == "" || $_POST['min'] == "" || $_POST['max'] == "") {
+    if (($_POST['leaseterm'] == "" || $_POST['uname'] == "" || $_POST['birthday'] == "" || $_POST['gender'] == "" || $_POST['income'] == "" || $_POST['apt_cat'] == "" || $_POST['movein'] == "" || $_POST['prev'] == "" || $_POST['min'] == "" || $_POST['max'] == "") {
         echo "<script> popup(); </script>";
     } else {
        $con = new mysqli($host,$username,$password,$database);
 	   
 	   $sql="INSERT INTO PROSPECTIVE_RESIDENT VALUES(
-			'test',
-			$_POST[uname],
-			$_POST[birthday],
-			$_POST[gender],
+			'user3',
+			'$_POST[uname]',
+			'$_POST[birthday]',
+			'$_POST[gender]',
+			'$_POST[leaseterm]',
+			'$_POST[apt_cat]',
+			'$_POST[movein]',
+			'$_POST[prev]',
 			$_POST[income],
-			$_POST[apt_cat],
-			$_POST[apt_cat],
-			$_POST[movein],
-			$_POST[prev],
 			$_POST[min],
 			$_POST[max]
 		);";

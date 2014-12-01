e53ef38387929a6adedb067af671debd461ec39d
Dan Holdridge <dholdridge3@gmail.com>
2014-12-01 13:15:35 -0500

    Created temporary 'homepage' and added functionality to add a credit card.

--- add_card.php
+++ add_card.php
@@ -1,8 +1,35 @@
+<?php require('connect_db.php'); ?>
 <html>
 	<head>
 		<title>Title</title>
 	</head>
 	<body>
-	
+		<?php
+			$name = $POST['cardholder'];
+			$num = $POST['cardnumber'];
+			$exp = $POST['expdate'];
+			$cvv = $POST['cvv'];
+			$user = "user1"; // Get the current user
+			if (! $name || ! $num || ! $exp || ! $cvv) {
+				echo "You didn't fill out all of the required information. Please try again.";
+				exit;
+			}
+			
+			$db = connect_db();
+			$query = "insert into PAYMENT_INFORMATION values($num, $cvv, $name, $user, $exp);";
+			echo $query;
+			
+			/*
+			$result = $db->query($query);
+			if ( $result) {
+				echo "Card Added";
+				}
+			else {
+				echo "Sorry, your card cannot be added at this time.";
+			}
+			$result->free();
+			*/
+			$db->close();
+		?>
 	</body>
 </html>

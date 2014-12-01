8c3f615b0a4413dbd113403cd59c53a77334d6c6
Dan Holdridge <dholdridge3@gmail.com>
2014-12-01 16:19:34 -0500

    Stuff. Fuck everything.

--- apt_allotment_result.php
+++ apt_allotment_result.php
@@ -4,6 +4,31 @@
 		<title>Title</title>
 	</head>
 	<body>
-	
+		<?php
+		/*Prospect becomes resident
+		Apt Availability = Move-in + Lease term
+		*/
+		$aptno = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+		$ = $_POST[''];
+
+		$db = connect_db();
+		$addRes = "insert into RESIDENT values();";
+		$updateApt = "update APARTMENT set Availabile_On = '' where Apt_No = '$aptno';	
+		$addResult = $db->query($addRes);
+		$updateResult = $db->query($updateApt);
+		
+
+		$addResult->free();
+		$updateResult->free();
+		$db->close();
+		?>
 	</body>
 </html>

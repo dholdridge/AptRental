c77fccc4af93a03dd291b13b0963bf0d1e80a3ba
Jamie <jleighface@gmail.com>
2014-12-02 15:29:41 -0500

    Update application.php
    rid of script code

--- application.php
+++ application.php
@@ -1,14 +1,7 @@
 <html>
 <center><h1>Prospective Resident Application Form</h1></center>
 <body>
-<script>
-function popup() {
-    alert("The form is not complete");
-}
-function popup2() {
-	alert("Application created successfully");
-}
-</script>
+
 
 <form action="" method="post">
 Name: <input type="text" name="uname"><br>

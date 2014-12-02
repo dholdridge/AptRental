a0b1bf6b710b28c5ae184169d6cfafc0a665757a
DaoGatech <DaoGatech@users.noreply.github.com>
2014-12-02 15:13:24 -0500

    Update application.php

--- application.php
+++ application.php
@@ -16,7 +16,7 @@ Birthday: <input type="date" name="birthday"><br>
 Gender: <select name="gender">
 <option value="M">M</option>
 <option value="F">F</option></select><br>
-Income: <input type="text" name="income"><br>
+Income: <input type="number" name="income"><br>
 Preferred Lease Term: <select name="leaseterm">
 <option value="3">3</option>
 <option value="6">6</option>
@@ -27,47 +27,13 @@ Apartment Category: <select name="apt_cat">
 <option value="2bdr-2bth">2bdr-2bth</option></select><br>
 Preferred Move In Date: <input type="date" name="movein"><br>
 Previous Residence: <input type="text" name="prev"><br>
-Rent Minimum: <input type="text" name="min"><br>
-Rent Maximum: <input type="text" name="max"><br>
+Rent Minimum: <input type="number" name="min"><br>
+Rent Maximum: <input type="number" name="max"><br>
 
-<input type="submit" name="submit" value="Create">
+<input type="submit" name="submit" value="Apply">
 </form>
 
 </body>
 </html>
 
-<?php
-include 'dbinfo.php' ; 
-if (isset($_POST['submit'])) {
-    if (($_POST['leaseterm'] == "" || $_POST['uname'] == "" || $_POST['birthday'] == "" || $_POST['gender'] == "" || $_POST['income'] == "" || $_POST['apt_cat'] == "" || $_POST['movein'] == "" || $_POST['prev'] == "" || $_POST['min'] == "" || $_POST['max'] == "")) {
-        echo "<script> popup(); </script>";
-    } else {
-       $con = new mysqli($host,$username,$password,$database);
-	   
-	   $sql="INSERT INTO PROSPECTIVE_RESIDENT VALUES(
-			'user3',
-			'$_POST[uname]',
-			'$_POST[birthday]',
-			'$_POST[gender]',
-			'$_POST[leaseterm]',
-			'$_POST[apt_cat]',
-			'$_POST[movein]',
-			'$_POST[prev]',
-			$_POST[income],
-			$_POST[min],
-			$_POST[max]
-		);";
-	    if ($con->connect_error) {
-			die("Connection failed: " . $con->connect_error);
-		} 
-		if ($con->query($sql) === TRUE) {
-			echo "<script>popup2();</script>";
-		} else {
-			echo "Error: " . $sql . "<br>" . $con->error;
-		}
 
-		$con->close();
-
-    }
-}
-?>
